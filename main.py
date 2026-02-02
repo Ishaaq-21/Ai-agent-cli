1121762c2fe3f2b9e3e2088464c2f28f91d7a3df
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-02-02 08:47:34 +0100

    feat(user_prompt): set the content of the genai to the passed userprompt

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
 
 resp = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    contents=args.user_prompt)
 
 
 if resp.usage_metadata is not None: 
-    print("Prompt tokens usage:", resp.usage_metadata.prompt_token_count)
-    print("Resp Tokens : ", resp.usage_metadata.candidates_token_count)
+    print("Prompt tokens:", resp.usage_metadata.prompt_token_count)
+    print("Response tokens: ", resp.usage_metadata.candidates_token_count)
 else: 
     raise RuntimeError("Api call failed")
 
 
-print("Reponse: \n", resp.text)
+print("Response: \n", resp.text)
