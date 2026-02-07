c9d556ba2e494521aeceb60785e50b8086912d42
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-02-07 21:31:00 +0100

    fix: change max_chars to 10000 and fix target path logic

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,14 +1,13 @@
 
 import os
 
-MAX_CHARS = 1000
+MAX_CHARS = 10000
 
 def get_file_content(working_directory, file_path):
     try: 
         working_dir_abs = os.path.abspath(working_directory)
-        target_path = os.path.abspath(file_path)
+        target_path = os.path.abspath(os.path.join(working_directory, file_path))
         is_outside = os.path.commonpath([working_dir_abs, target_path]) != working_dir_abs
-
         if is_outside:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         if not os.path.isfile(target_path):
