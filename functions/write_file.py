c0fb1e0bd0ef95be571be762c4189552e05cfb4e
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-02-07 21:47:11 +0100

    feat: write the function that will make the agent able to write files

---
+++ functions/write_file.py
@@ -0,0 +1,21 @@
+
+import os
+
+
+def write_file(working_directory, file_path, content):
+        try: 
+            working_dir_abs = os.path.abspath(working_directory)
+            target_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+            is_outside = os.path.commonpath([working_dir_abs, target_path]) != working_dir_abs
+            if is_outside:
+                return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+            
+            if os.path.isdir(target_path):
+                return f'Error: Cannot write to "{file_path}" as it is a directory'
+        
+            with open(target_path, 'w') as f:
+                f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        except Exception as e:
+            return f'Error: {str(e)}'
