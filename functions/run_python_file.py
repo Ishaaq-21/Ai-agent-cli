56d83fe27f023a3a65d670fb7d5cde47babfaea2
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-02-07 22:42:15 +0100

    add function that runs python files as second process

---
+++ functions/run_python_file.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+def run_python_file(working_directory, file_path, args=None):
+    try: 
+        working_dir_abs = os.path.abspath(working_directory)
+        target_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+        is_outside = os.path.commonpath([working_dir_abs, target_path]) != working_dir_abs
+        if is_outside:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+
+        if not os.path.isfile(target_path):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        
+        if not target_path.endswith('.py'):
+            return f'Error: Cannot execute "{file_path}" is not a Python file'
+
+        command = ["python3", target_path]
+        if args:
+            command.extend(args)
+        
+        result = subprocess.run(command, capture_output=True, text=True, timeout=30)
+
+        parts = []
+        if result.returncode != 0: 
+            parts.append(f"Process exited with code {result.returncode}")
+        
+        stdout_txt = result.stdout.strip() if result.stdout else ""
+        stderr_txt = result.stderr.strip() if result.stderr else ""
+
+        if not stdout_txt and not stderr_txt:
+            parts.append("No output produced")
+        else: 
+            if stdout_txt:
+                parts.append(f"STDOUT:\n{stdout_txt}")
+            if stderr_txt:
+                parts.append(f"STDERR:\n{stderr_txt}")
+
+        return "\n".join(parts)
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
