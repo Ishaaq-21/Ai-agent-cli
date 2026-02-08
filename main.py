81b0eb48734552b384b22810a48acbb41eedc2c2
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-02-08 21:10:57 +0100

    feat: Add verbose argument to our tool

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+# --verbose flag
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
