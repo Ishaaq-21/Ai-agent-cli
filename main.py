3eac537af910eab77f187673eb5d3dadc3a398ad
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-02-10 15:37:08 +0100

    give our llm the ability to call the available functions defined back using types.tools

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from google import genai
 import argparse
 from google.genai import types
+from prompts import system_prompt
+from call_functions import available_functions
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -10,6 +12,7 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
+#user prompt argument
 parser.add_argument("user_prompt", type=str, help="User prompt")
 # --verbose flag
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
@@ -21,7 +24,9 @@ messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)]
 
 resp = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=messages)
+    contents= args.user_prompt, 
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature= 0),
+    )
 
 if args.verbose:
     if resp.usage_metadata is not None:
@@ -31,11 +36,4 @@ if args.verbose:
     else:
         raise RuntimeError("Api call failed")
 
-if resp.usage_metadata is not None: 
-    print("Prompt tokens:", resp.usage_metadata.prompt_token_count)
-    print("Response tokens: ", resp.usage_metadata.candidates_token_count)
-else: 
-    raise RuntimeError("Api call failed")
-
-
-print("Response: \n", resp.text)
+print(resp.text)
